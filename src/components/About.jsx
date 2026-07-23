a2f58b2183ec782aef5d7cc9b2ca2cf1754fbcb8
adarshrajshah04 <adarshrajshah04@gmail.com>
2026-07-23 19:49:39 +0530

    Update About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,14 +16,18 @@ const About = () => {
 
         <Row>
             <Col>
-            
+                <b style={{color:'var(--green)'}}><i class="bi bi-stars"></i>Built For Modern Teams</b>
+                <h3 className='heading mt-4 fw-bolder' style={{fontSize:'40px'}}>Ship Beautiful Products at Lightning Speed</h3>
+                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+
+                ``
             </Col>
             <Col>
             <div className={classes.mac_parent}>
                 <div className={classes.mac_btn}>
-                    <div className='rounded-pill' style={{width:'20px',height:'20px', background:'red'}}></div>
-                    <div className='rounded-pill' style={{width:'20px',height:'20px', background:'yellow'}}></div>
-                    <div className='rounded-pill' style={{width:'20px',height:'20px', background:'green'}}></div>
+                    <div className='rounded-pill' style={{width:'15px',height:'15px', background:'red'}}></div>
+                    <div className='rounded-pill' style={{width:'15px',height:'15px', background:'yellow'}}></div>
+                    <div className='rounded-pill' style={{width:'15px',height:'15px', background:'green'}}></div>
                   
                 </div>
                 <img src={About_img} alt=""style={{width:'100%'}} />
