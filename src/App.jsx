7e4dbd208d998e3e406a2c1c89ae48496c24ad79
adarshrajshah04 <adarshrajshah04@gmail.com>
2026-07-30 12:40:04 +0530

    change something

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,28 @@ import About from './components/About/About.jsx';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio.jsx';
 import Team from './components/Team/Team.jsx';
+import Contact from './components/Contact.jsx';
+
 
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Contact from './components/Contact.jsx';
+import { Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div>
       <Navigation/>
-      <Hero/>
-      <About/>
-      <Services/>
-      <Portfolio/>
-      <Team/>
-      <Contact/>
+      <Routes>
+       <Route index element={<Hero/>}/>
+       <Route path='/Home' element={<Hero/>}/>
+       <Route path='/About' element={<About/>}/>
+       <Route path='/Services' element={<Services/>}/>
+       <Route path='/Portfolio' element={<Portfolio/>}/>
+       <Route path='/Team' element={<Team/>}/>
+       <Route path='/Contact' element={<Contact/>}/>
+      </Routes>
     </div>
   )
 }
--- src/components/global/Navigation.jsx
+++ src/components/global/Navigation.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import Button from "../Button";
+import { Link } from "react-router-dom";
+import classes from '../../assets/css/Naveigation.module.css'
+
 
 const Navigation = () => {
     return (
-        <div>
+        <div >
             <Container data-bs-theme="dark" >
-                <Navbar expand="lg" className="navbar px-5 py-3 rounded-pill " >
+                <Navbar expand="lg" className={`navbar px-5 py-3 rounded-pill `} >
                     <Container>
                         <Navbar.Brand href="#home"><i className="bi bi-shadows me-2"></i>Rival</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="m-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#home">About</Nav.Link>
-                                <Nav.Link href="#home">Services</Nav.Link>
-                                <Nav.Link href="#home">Portfolio</Nav.Link>
-                                <Nav.Link href="#home">Team</Nav.Link>
-                                <Nav.Link href="#home">Contact</Nav.Link>
+                            <Nav className={`m-auto ${classes.nav}`}>
+                                <Link className={classes.Link} to='/Home'>Home</Link>
+                                <Link className={classes.Link} to='/About'>About</Link>
+                                <Link className={classes.Link} to='/Services'>Services</Link>
+                                <Link className={classes.Link} to='/Portfolio'>Portfolio</Link>
+                                <Link className={classes.Link} to='/Team'>Team</Link>
+                                <Link  className={classes.Link} to='/Contact'>Contact</Link>
                                 
 
                             </Nav>
